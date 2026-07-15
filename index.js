8be537eb70ac3e008c488f709e6c0e1081600c61
KIWOOM-업무망\kiwoom <parkgyu1984@gmail.com>
2026-07-15 13:41:59 +0900

    키움 REST API전문 형식으로 웹훅 메시지 변경

--- index.js
+++ index.js
@@ -34,21 +34,24 @@ app.post('/kiwoom/webhook/kr/buy', (req, res) => {
     if(req.body) {
         // 트레이딩 뷰 웹훅 알림 메시지 수신
         let tradingViewData = JSON.parse(req.body);
+        let dmstStexTp = tradingViewData.dmstStexTp;  // 국내거래소구분코드
         let strCd = tradingViewData.strCd;  // 종목코드
-        let ordUv = tradingViewData.close;  // 현재가
-        let volume = tradingViewData.volume;  // 거래량
+        let ordUv = tradingViewData.ordUv;  // 현재가
+        let ordQty = tradingViewData.ordQty;  // 수량
+        let trdeTp = tradingViewData.trdeTp;    // 매매구분
+        let condUv = tradingViewData.condUv || '';    // 조건단가
 
         // 현재가가 호가 단위에 맞지 않게 들어오는 경우가 있어서 보정 처리 함
         let fixOrdUv = common.correctKrPrice(ordUv);
 
         // 키움 REST API로 주문 실행
         let params = {
-            'dmst_stex_tp' : 'KRX', // KRX,NXT,SOR(모의투자는KRX)
+            'dmst_stex_tp' : dmstStexTp, // KRX,NXT,SOR(모의투자는KRX)
             'stk_cd' : strCd,
-            'ord_qty' : '1',    // 매수수량
+            'ord_qty' : ordQty,    // 매수수량
             'ord_uv' : fixOrdUv,
-            'trde_tp' : '0',    //  0:보통 , 3:시장가 , 5:조건부지정가 , 81:장마감후시간외 , 61:장시작전시간외, 62:시간외단일가 , 6:최유리지정가 , 7:최우선지정가 , 10:보통(IOC) , 13:시장가(IOC) , 16:최유리(IOC) , 20:보통(FOK) , 23:시장가(FOK) , 26:최유리(FOK) , 28:스톱지정가,29:중간가,30:중간가(IOC),31:중간가(FOK)
-            'cond_uv' : ''
+            'trde_tp' : trdeTp,    //  0:보통 , 3:시장가 , 5:조건부지정가 , 81:장마감후시간외 , 61:장시작전시간외, 62:시간외단일가 , 6:최유리지정가 , 7:최우선지정가 , 10:보통(IOC) , 13:시장가(IOC) , 16:최유리(IOC) , 20:보통(FOK) , 23:시장가(FOK) , 26:최유리(FOK) , 28:스톱지정가,29:중간가,30:중간가(IOC),31:중간가(FOK)
+            'cond_uv' : condUv
         };
         apiCaller.krOrdByu(params);
     } else {
@@ -68,21 +71,24 @@ app.post('/kiwoom/webhook/kr/sell', (req, res) => {
     if(req.body) {
         // 트레이딩 뷰 웹훅 알림 메시지 수신
         let tradingViewData = JSON.parse(req.body);
+        let dmstStexTp = tradingViewData.dmstStexTp;  // 국내거래소구분코드
         let strCd = tradingViewData.strCd;  // 종목코드
-        let ordUv = tradingViewData.close;  // 현재가
-        let volume = tradingViewData.volume;  // 거래량
+        let ordUv = tradingViewData.ordUv;  // 현재가
+        let ordQty = tradingViewData.ordQty;  // 수량
+        let trdeTp = tradingViewData.trdeTp;    // 매매구분
+        let condUv = tradingViewData.condUv || '';    // 조건단가
 
         // 현재가가 호가 단위에 맞지 않게 들어오는 경우가 있어서 보정 처리 함
         let fixOrdUv = common.correctKrPrice(ordUv);
 
         // 키움 REST API로 주문 실행
         let params = {
-            'dmst_stex_tp' : 'KRX', // KRX,NXT,SOR(모의투자는KRX)
+            'dmst_stex_tp' : dmstStexTp, // KRX,NXT,SOR(모의투자는KRX)
             'stk_cd' : strCd,
-            'ord_qty' : '1',    // 매도수량
+            'ord_qty' : ordQty,    // 매도수량
             'ord_uv' : fixOrdUv,
-            'trde_tp' : '0',    // 0:보통 , 3:시장가 , 5:조건부지정가 , 81:장마감후시간외 , 61:장시작전시간외, 62:시간외단일가 , 6:최유리지정가 , 7:최우선지정가 , 10:보통(IOC) , 13:시장가(IOC) , 16:최유리(IOC) , 20:보통(FOK) , 23:시장가(FOK) , 26:최유리(FOK) , 28:스톱지정가,29:중간가,30:중간가(IOC),31:중간가(FOK)
-            'cond_uv' : ''
+            'trde_tp' : trdeTp,    // 0:보통 , 3:시장가 , 5:조건부지정가 , 81:장마감후시간외 , 61:장시작전시간외, 62:시간외단일가 , 6:최유리지정가 , 7:최우선지정가 , 10:보통(IOC) , 13:시장가(IOC) , 16:최유리(IOC) , 20:보통(FOK) , 23:시장가(FOK) , 26:최유리(FOK) , 28:스톱지정가,29:중간가,30:중간가(IOC),31:중간가(FOK)
+            'cond_uv' : condUv
         };
         apiCaller.krOrdSell(params);
     } else {
@@ -102,18 +108,20 @@ app.post('/kiwoom/webhook/us/buy', (req, res) => {
     if(req.body) {
         // 트레이딩 뷰 웹훅 알림 메시지 수신
         let tradingViewData = JSON.parse(req.body);
-        let stexTp = tradingViewData.stexTp;  // 거래소구분
+        let stexTp = tradingViewData.stexTp;  // 미국래소구분코드
         let strCd = tradingViewData.strCd;  // 종목코드
-        let ordUv = tradingViewData.close;  // 현재가
-        let volume = tradingViewData.volume;  // 거래량
+        let ordUv = tradingViewData.ordUv;  // 현재가
+        let ordQty = tradingViewData.ordQty;  // 수량
+        let trdeTp = tradingViewData.trdeTp;    // 매매구분
+        let stopPric = tradingViewData.stopPric || '';    // STOP가격
 
         // 키움 REST API로 주문 실행
         let params = {
             'stex_tp' : stexTp,   // NA: AMEX, ND: NASDAQ, NY: NYSE
             'stk_cd' : strCd,
-            'ord_qty' : '1',    // 매수수량
+            'ord_qty' : ordQty,    // 매수수량
             'ord_uv' : ordUv, // trde_tp가 00(지정가),30(LOC)...인 경우 필수 입력, 그 외 시장가 거래유형 설정 시 입력 값은 빈 값 처리
-            'trde_tp' : '00'    // 00:지정가 03시장가 26:VWAP지정가 27:TWAP지정가 30:LOC 36:VWAP시장가 37:TWAP시장가
+            'trde_tp' : trdeTp    // 00:지정가 03시장가 26:VWAP지정가 27:TWAP지정가 30:LOC 36:VWAP시장가 37:TWAP시장가
         };
         apiCaller.usOrdByu(params);
     } else {
@@ -133,18 +141,20 @@ app.post('/kiwoom/webhook/us/sell', (req, res) => {
     if(req.body) {
         // 트레이딩 뷰 웹훅 알림 메시지 수신
         let tradingViewData = JSON.parse(req.body);
-        let stexTp = tradingViewData.stexTp;  // 거래소구분
+        let stexTp = tradingViewData.stexTp;  // 미국래소구분코드
         let strCd = tradingViewData.strCd;  // 종목코드
-        let ordUv = tradingViewData.close;  // 현재가
-        let volume = tradingViewData.volume;  // 거래량
+        let ordUv = tradingViewData.ordUv;  // 현재가
+        let ordQty = tradingViewData.ordQty;  // 수량
+        let trdeTp = tradingViewData.trdeTp;    // 매매구분
+        let stopPric = tradingViewData.stopPric || '';    // STOP가격
 
         // 키움 REST API로 주문 실행
         let params = {
             'stk_cd' : strCd,  // NVDA
             'stex_tp' : stexTp,   // NA: AMEX, ND: NASDAQ, NY: NYSE
-            'ord_qty' : '1',      // 매도수량
+            'ord_qty' : ordQty,      // 매도수량
             'ord_uv' : ordUv, // trde_tp가 00(지정가),30(LOC)...인 경우 필수 입력, 그 외 시장가 거래유형 설정 시 입력 값은 빈 값 처리
-            'stop_pric' : '',   // trde_tp가 34(STOP LIMIT) 또는 35(STOP)인 경우 필수 입력, 그 외 거래유형(지정가,시장가 등) 설정 시 입력 값은 무시되거나 빈 값처리.
+            'stop_pric' : stopPric,   // trde_tp가 34(STOP LIMIT) 또는 35(STOP)인 경우 필수 입력, 그 외 거래유형(지정가,시장가 등) 설정 시 입력 값은 무시되거나 빈 값처리.
             'trde_tp' : '00'    // 00:지정가 03시장가 26:VWAP지정가 27:TWAP지정가 30:LOC 33:MOC 36:VWAP시장가 37:TWAP시장가 35:STOP 34:STOP LIMIT
         };
         apiCaller.usOrdSell(params);
